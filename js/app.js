2ada849c90e2dec1676e47836da7c49c3e1e76e0
Apple-supaporn <spk.apple@gmail.com>
2023-07-05 23:19:34 -0700

    completed html and css

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("Sanity Check");
+
+
+//function startGame() : to start the game.
+//function playSequence() : to plays the sequence of colors and sounds.
+//function highlightColor() : to help with the sequence of the game.
+//function buttonClickHandler() : to be able to follow the sequence. 
+//function checkSequence() : to compares user's input if it matches.
+//function startNextRound() : to start next round after user successfully repeats the sequence.
+//function endGame() : game end once player makes mistake. 
+//function score() : to see my highest score.
+//function highestScore : to see highest score. 
+//function replyGame() :  to replay game after making a mistake.
+//function muteSound() :  to be able to mute the sound effects
