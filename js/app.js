936a6a4fa0ee1a12bde52e81c7fe71cc83c562ce
Apple-supaporn <spk.apple@gmail.com>
2023-07-13 00:39:54 -0700

    completed rearrange the code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-let previousHighestScore = parseInt(localStorage.getItem("highestScore"));
-    if (isNaN(previousHighestScore)){
-    previousHighestScore = 0;
-    }
-let previousHighestScoreEl = document.getElementById("previousHighestScore");
-    previousHighestScoreEl.innerText = previousHighestScore.toString();
-
-
-//create an object colors and add value for their corresponding
+//GAME VARIABLES
 const colors = { green: 0, red: 1, blue: 2, yellow: 3 };
-let gameSequence = [];    //store the sequence by game
-let userSequence = [];    //store the user's sequence input
-let gamePlaying = false;  //flag if the game is playing
-let userPlaying = false;  //flag if user is playing
+let gameSequence = [];
+let userSequence = [];
+let gamePlaying = false;
+let userPlaying = false;
 let gameOver = false;
 let isMuted = false;
+let currentHighestScore = 0;
 
+
+//SOUND URLS AND AUDIO ELEMENTS
 const soundUrls = [
-    'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',  
-    'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
-    'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
-    'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
+  'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',  
+  'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
 ];
- 
+
+
 const sounds = soundUrls.map(url => {
-    const audio = new Audio(url);
-    audio.load();
-    return audio;
+  const audio = new Audio(url);
+  audio.load();
+  return audio;
 });
 
 
@@ -35,109 +31,101 @@ const redPanel = document.getElementById('redPanel');
 const bluePanel = document.getElementById('bluePanel');
 const yellowPanel = document.getElementById('yellowPanel');
 
+
 //DOM VARIABLE FOR BUTTONS
 const soundButton = document.getElementById('soundButton');
 const howtoplayButton = document.getElementById('howtoplayButton');
 const replayButton = document.getElementById('replayButton');
 const playButton = document.getElementById('playButton');
-
-//DOM VARIABLE FOR SCORE DISPLAYS
-const score = document.getElementById('score');
-const highestScore = document.getElementById('highestScore');
-
 const instructionPopup = document.getElementById('instructionPopup');
 const resetHighestScoreButton = document.getElementById('resetHighestScoreButton');
 const quitButton = document.getElementById('quitButton');
 
 
+//DOM VARIABLE FOR SCORE DISPLAYS
+const score = document.getElementById('score');
+const highestScore = document.getElementById('highestScore');
+let previousHighestScore = parseInt(localStorage.getItem("highestScore"));
+if (isNaN(previousHighestScore)) {
+  previousHighestScore = 0;
+};
+let previousHighestScoreEl = document.getElementById("previousHighestScore");
+previousHighestScoreEl.innerText = previousHighestScore.toString();
 
-//function to start the game
-function startGame() {    //reset the game from begining
-    gameOver = false;     //set before star
-    gameSequence = [];
-    userSequence = [];
-    gamePlaying = true;
-    userPlaying = false;
-    currentScore = 0;
-    currentHighestScore = parseInt(localStorage.getItem("highestScore")) || 0;
-    score.innerText = `Score: 0`;
-    highestScore.innerText = `Your Highest Score: ${currentHighestScore}`;
-    console.log('Game satrted')
-    startNextRound();
-    playButton.innerText = "";
+
+function startGame() {
+  gameOver = false;
+  gameSequence = [];
+  userSequence = [];
+  gamePlaying = true;
+  userPlaying = false;
+  currentScore = 0;
+  currentHighestScore = parseInt(localStorage.getItem("highestScore")) || 0;
+  score.innerText = `Score: 0`;
+  highestScore.innerText = `Your Highest Score: ${currentHighestScore}`;
+  startNextRound();
+  playButton.innerText = "";
 };
 
 
-
-//function to start next round
-function startNextRound() {                             //when startNextRound is called
-    userSequence = [];                                  //first reset the userSequence array
-    const nextColor = Math.floor(Math.random() * 4);    //generate the next color for the gameSequence by using Math.random()
-    gameSequence.push(nextColor);                       //push it into gameSequence array, then call playSequence
-    console.log('Next color: ', nextColor)
-    playSequence();
+function startNextRound() {
+  userSequence = [];
+  const nextColor = Math.floor(Math.random() * 4);
+  gameSequence.push(nextColor);
+  playSequence();
 };
 
 
-
-//function to play the generate sequence
 function playSequence() {
-    userPlaying = false                                 //stop user input while the sequence is playing
-    let i = 0;                                          //set a variable i to keep track of index in the gameSequence
-    const intervalId = setInterval(() => {
-        const color = gameSequence[i];
-        highlightColor(color);                          //highlight the current color in the sequence
-        if(!isMuted){
-          playSound(color);
-        }   
-        console.log('Playing color:', color)            //play the sound
-        i++;                                            //move to the next color in the sequence
-    
-        if(i === gameSequence.length){                  //if all colors have been played, then user turn.
-        clearInterval(intervalId);
-            setTimeout(() => {                          //clear user sequence after show all of the sequence
-            userSequence = [];
-            userPlaying = true;
-            console.log('Game sequence', gameSequence)
-            }, 0);                                      //set timeout to 0 so the array is cleared immediately after the sequence finishes playing. 
-        };
-    }, 750);
+  userPlaying = false;
+  let i = 0;
+  const intervalId = setInterval(() => {
+    const color = gameSequence[i];
+    highlightColor(color);
+    if(!isMuted) {
+      playSound(color);
+    };
+    i++;
+    if(i === gameSequence.length) {
+      clearInterval(intervalId);
+      setTimeout(() => {
+        userSequence = [];
+        userPlaying = true;
+      }, 0);
+    };
+  }, 750);
 };
 
 
-
-//function to highlight colors
-function highlightColor(color) {                       //use switch statement 
-    switch (color) {
-      case colors.green:
-        greenPanel.classList.add('active');
-        setTimeout(() => {
+function highlightColor(color) {
+  switch (color) {
+    case colors.green:
+      greenPanel.classList.add('active');
+      setTimeout(() => {
         greenPanel.classList.remove('active');
-        }, 500);
-        break;
-
-      case colors.red:
-        redPanel.classList.add('active');
-        setTimeout(() => {
+      }, 500);
+      break;
+    case colors.red:
+      redPanel.classList.add('active');
+      setTimeout(() => {
         redPanel.classList.remove('active');
-        }, 500);
-        break;
+      }, 500);
+      break;
       
-      case colors.blue:
-        bluePanel.classList.add('active');
-        setTimeout(() => {
+    case colors.blue:
+      bluePanel.classList.add('active');
+      setTimeout(() => {
         bluePanel.classList.remove('active');
-        }, 500);
-        break;
-
-      case colors.yellow:
-        yellowPanel.classList.add('active');
-        setTimeout(() => {
+      }, 500);
+      break;
+    case colors.yellow:
+      yellowPanel.classList.add('active');
+      setTimeout(() => {
         yellowPanel.classList.remove('active');
-        }, 500);
-        break;
-    }
-}
+      }, 500);
+      break;
+  };
+};
 
 
 function playSound(color) {
@@ -147,107 +135,88 @@ function playSound(color) {
 
 function checkSequence() {
   for (let i = 0; i < userSequence.length; i++) {
-      if(userSequence[i] !== gameSequence[i]) {
-        console.log('Sequence Index', i, 'Correct Color', gameSequence[i], 'User Clicked', userSequence[i])
-        return false;                                     //return false immediately when userSequence doesn't match with gameSequence. 
-      }
-  }
+    if(userSequence[i] !== gameSequence[i]) {
+      return false;
+    };
+  };
   return true;
-}
+};
 
 
 function updateScore() {
   const currentScore = userSequence.length;
   if (currentScore > currentHighestScore) {
-      currentHighestScore = currentScore; 
-      localStorage.setItem("highestScore", currentHighestScore.toString());
-      console.log('Current score', currentScore)
-  }
+    currentHighestScore = currentScore; 
+    localStorage.setItem("highestScore", currentHighestScore.toString());
+  };
   score.innerText = `Score: ${currentScore}`;
   highestScore.innerText = `Your Highest Score: ${currentHighestScore}`;
 };
 
 
-
 function endGame() {
-  if(gameOver){
-      return;
-  }
+  if(gameOver) {
+    return;
+  };
   gamePlaying = false;
   userPlaying = false;
   gameOver = true;
-
   const gameOverMessage = document.getElementById('endGameMessage');
   gameOverMessage.innerText = "GAME OVER";
-
   const gameOverScreen = document.getElementById('gameOver');
   gameOverScreen.style.display = 'block';
-
   const replayButton = document.getElementById('replayButton');
   replayButton.addEventListener('click', handleReply);
-
   replayButton.addEventListener('click', () => {
     replayButton.removeEventListener('click', handleReply);
     score.innerText = 'Score: 0';
     handleReply();
-  })
+  });
 };
-
 
 
 function handleReply() {
   startGame();
-
   const gameOverMessage = document.getElementById('endGameMessage');
   gameOverMessage.innerText = "";
-
   const gameOverScreen = document.getElementById('gameOver');
   gameOverScreen.style.display = 'none';
-
   const replayButton = document.getElementById('replayButton');
   replayButton.removeEventListener('click', handleReply);
-
-}
-
+};
 
 
-function updateHighestScore(newScore){
+function updateHighestScore(newScore) {
   if (newScore > currentHighestScore) {
-      currentHighestScore = newScore;
-      localStorage.setItem('highestScore', currentHighestScore.toString()); //to store a value in the browser's local storage  and .toString is to convert the value to a string
-      highestScore.innerText = `Your Highest Score: ${currentHighestScore}`;
-      console.log('highest score', highestScore)
-  }
-}
-
+    currentHighestScore = newScore;
+    localStorage.setItem('highestScore', currentHighestScore.toString());
+    highestScore.innerText = `Your Highest Score: ${currentHighestScore}`;
+  };
+};
 
 
 function handlePanelClick(color) {
-    if (userPlaying) { //since userplaying is already being check, the gameplaying check is unnecessery. 
-        userSequence.push(color);
-        if(!isMuted){
-          playSound(color);
-        }
-        highlightColor(color);
-            if(!checkSequence()) {                                         //check if user's sequence is incorrect
-            endGame();                                                     //endGame if the user's sequence doesn't match
-            } else if (userSequence.length === gameSequence.length) { 
-                updateScore();                                             // if matched, update score, reset the user to false, and start next round
-                userPlaying = false;
-                console.log('User playing', userPlaying)
-                setTimeout(startNextRound, 500);
-              };
+  if (userPlaying) {
+    userSequence.push(color);
+    if(!isMuted){
+      playSound(color);
+    }
+    highlightColor(color);
+    if(!checkSequence()) {
+      endGame();
+    } else if (userSequence.length === gameSequence.length) { 
+      updateScore();
+      userPlaying = false;
+      setTimeout(startNextRound, 500);
     };
+  };
 };
-
 
 
 function muteSound() {
-  const sounds = document.querySelectorAll('audio')
+  const sounds = document.querySelectorAll('audio');
   isMuted = !isMuted;
-  console.log(isMuted);
 };
-
 
 
 function openInstruction() {
@@ -255,66 +224,69 @@ function openInstruction() {
 };
 
 
-
 function closeInstruction() {
   instructionPopup.style.display = 'none';
-}
+};
+
 
 function resetHighestScore() {
   localStorage.removeItem("highestScore");
   previousHighestScore = 0;
   previousHighestScoreEl.innerText = previousHighestScore.toString();
   highestScore.innerText = `Your Highest Score: ${previousHighestScore}`;
-}
+};
 
-function quitGame(){
+
+function quitGame() {
   gamePlaying = false;
   userPlaying = false;
   gameOver = false;
-
-  const gameOverScreen = document.getElementById('gameOver')
+  const gameOverScreen = document.getElementById('gameOver');
   gameOverScreen.style.display = 'none';
-
   score.innerText = 'Score: 0';
   playButton.innerText = "Play";
-}
+};
 
 
-//ADD EVENT LISTENER
+//EVENT LISTENERS
+howtoplayButton.addEventListener('click', openInstruction);
+resetHighestScoreButton.addEventListener('click', resetHighestScore);
+replayButton.addEventListener('click', startGame);
+quitButton.addEventListener('click', quitGame);
+playButton.addEventListener('click', startGame);
+
+greenPanel.addEventListener('click', () => {
+  if(userPlaying){
+    handlePanelClick(colors.green);
+  };
+});
+
+redPanel.addEventListener('click', () => {
+  if(userPlaying){
+    handlePanelClick(colors.red);
+  };
+});
+
+bluePanel.addEventListener('click', () => {
+  if(userPlaying){
+    handlePanelClick(colors.blue);
+  };
+});
+
+yellowPanel.addEventListener('click', () => {
+  if(userPlaying){
+    handlePanelClick(colors.yellow);
+  };
+});
+
 soundButton.addEventListener('click', () => {
   muteSound();
   if (isMuted) {
     soundButton.textContent = "Sound Off";
   } else {
     soundButton.textContent = "Sound On";
-  }
-});
-howtoplayButton.addEventListener('click', openInstruction);
-resetHighestScoreButton.addEventListener('click', resetHighestScore)
-replayButton.addEventListener('click', startGame);
-quitButton.addEventListener('click', quitGame);
-playButton.addEventListener('click', startGame);
-
-greenPanel.addEventListener('click', () => {
-    if(userPlaying){
-      handlePanelClick(colors.green);
-    };
+  };
 });
 
-redPanel.addEventListener('click', () => {
-    if(userPlaying){
-      handlePanelClick(colors.red);
-    };
-});
 
-bluePanel.addEventListener('click', () => {
-    if(userPlaying){
-      handlePanelClick(colors.blue);
-    };
-});
 
-yellowPanel.addEventListener('click', () => {
-    if(userPlaying){
-      handlePanelClick(colors.yellow);
-    };
-});
